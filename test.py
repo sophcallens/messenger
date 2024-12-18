6797b4eb8b58be9f354d0758cb73b4e94b459111
Sophie Callens <soph.callens@gmail.com>
2024-12-18 16:56:12 +0100

    suppression fichiers inutiles

--- test.py
+++
@@ -1,5 +0,0 @@
-# Demander une saisie utilisateur et faire un saut de ligne
-reponse = input("Entrez quelque chose : \n")
-
-# Afficher le message en dessous de l'input
-print("Voici un message affiché après l'input.")
